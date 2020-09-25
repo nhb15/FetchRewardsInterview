c5107de68e8bfebb05fcf0b8c6b73a668a6b2285
Nate Boldt <nate.h.boldt@gmail.com>
2020-09-25 10:41:19 -0500

    Initializing Spring project

---
+++ src/main/java/com/nhb15/FetchRewardsPyramid/FetchRewardsPyramidServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nhb15.FetchRewardsPyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchRewardsPyramidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchRewardsPyramidServiceApplication.class, args);
+	}
+
+	
+
+}
---
+++ src/test/java/com/nhb15/FetchRewardsPyramid/FetchRewardsPyramidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhb15.FetchRewardsPyramid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsPyramidServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
