16eb97289a2cbfa3c0f805b02df4cb79cbd8a59b
Nate Boldt <nate.h.boldt@gmail.com>
2020-09-25 13:13:29 -0500

    Completed core algorithm

--- src/main/java/com/nhb15/FetchRewardsPyramid/controller/PyramidController.java
+++ src/main/java/com/nhb15/FetchRewardsPyramid/controller/PyramidController.java
@@ -3,13 +3,26 @@ package com.nhb15.FetchRewardsPyramid.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.TreeSet;
+
 @RestController
 public class PyramidController {
-
     @GetMapping(path="isPyramid/{inputWord}")
     @ResponseStatus(value= HttpStatus.OK)
     public boolean isPyramid(@PathVariable String inputWord){
-        return true;
+
+        HashMap<Character, Integer> charFrequencyMap = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < inputWord.length(); i++){
+            charFrequencyMap.merge(inputWord.charAt(i), 1, Integer::sum);
+        }
+
+        //Tree set is automatically sorted in ascending order
+        TreeSet<Integer> characterFreqSet = new TreeSet<>(charFrequencyMap.values());
+
+        //If the set size matches the map size, all values in the map were distinct. If the last value is greater than the number of the distinct characters, the set skipped a value and should be false.
+        return (charFrequencyMap.size() == characterFreqSet.size() && characterFreqSet.last() <= characterFreqSet.size());
     }
 
 }
